6cf724bc3d74f80ca70f3e7574e36bc0b0dbda38
cslee <cslee@fondee.io>
2023-08-27 21:36:11 +0900

    feat 게시글 디테일 페이지 생성
    - boards 인덱스 페이지에서 디테일 페이지 연결
    - boards 디테일 페이지 부트스트랩 스타일 적용

--- boards/urls.py
+++ boards/urls.py
@@ -5,5 +5,7 @@ from boards import views
 app_name = "boards"
 urlpatterns = [
     # ex: /boards
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # ex: /boards/1
+    path("/<int:post_id>", views.detail, name="detail"),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,15 +1,25 @@
 import typing
-from django.shortcuts import render
-from boards import models
+
+from django.shortcuts import render, get_object_or_404
+from boards.models import Post, PostComment
 
 if typing.TYPE_CHECKING:
+    from django.http import HttpRequest
     from django.shortcuts import HttpResponse
     from django.db.models import QuerySet
 
 
 def index(request) -> 'HttpResponse':
-    posts: 'QuerySet[models.Post]' = models.Post.objects.all()
+    posts: 'QuerySet[Post]' = Post.objects.all()
     context = {
         "posts": posts
     }
     return render(request, 'boards/index.html', context)
+
+
+def detail(request: 'HttpRequest', post_id: int) -> 'HttpResponse':
+    post: Post = get_object_or_404(Post, pk=post_id)
+    context = {
+        "post": post
+    }
+    return render(request, 'boards/detail.html', context)
